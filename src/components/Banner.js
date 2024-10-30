bcba244a2eba018cab13bb26ef1136e22d159bc2
Minyi Liu <minyi.liu@berkeley.edu>
2024-10-29 22:31:40 -0700

    resume update

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -12,7 +12,7 @@ export const Banner = () => {
   const [text, setText] = useState('');
   const [delta, setDelta] = useState(300 - Math.random() * 100);
   const [index, setIndex] = useState(1);
-  const toRotate = [ "Software Engineer", "Cal Sophomore", "Full-Stack Developer" ];
+  const toRotate = [ "Software Engineer", "Cal Junior", "ML Engineer" ];
   const period = 1500;
 
   useEffect(() => {
